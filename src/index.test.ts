a01d6c4cafd7089864f1df8b64070cfb550fd26d
Peng Boris <peng@traleor.com>
2023-10-21 04:38:12 +0100

    wrote complete test for index

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,6 @@
-import { CMSClient } from "./index"; // Adjust the path as needed
-import { CMSContentPath, CMSQueries } from "@/types";
+import { CMSClient } from "./index";
+import { CMSContentPath, CMSMediaMeta, CMSQueries } from "@/types";
 import { FetchError } from "@/lib";
-import { buildQueryString } from "@/utils";
 import { fetchContent } from "@/lib";
 
 // Mock the fetchContent module
@@ -97,5 +96,243 @@ describe("CMSClient", () => {
       { Authorization: "Bearer token" },
       cache
     );
+  });
+
+  it("should fetch a single page based on ID or slug", async () => {
+    // Mock the fetchContent function to return a successful response
+    const resp = {
+      meta: { total_count: 1 },
+      items: [{ id: "1" }],
+    };
+    mockFetchContent.mockResolvedValue(resp);
+
+    const idOrSlug = "example-slug";
+    const response = await client.fetchPage(idOrSlug, queries);
+
+    expect(response).toEqual(resp.items[0]);
+
+    // Ensure that fetchContent was called with the correct arguments
+    expect(fetchContent).toHaveBeenCalledWith(
+      baseURL,
+      apiPath,
+      "pages",
+      { slug: idOrSlug, ...queries },
+      { Authorization: "Bearer token" },
+      cache
+    );
+  });
+
+  it("should fetch a single image based on ID or slug", async () => {
+    // Mock the fetchContent function to return a successful response
+    const resp = {
+      meta: { total_count: 1 },
+      items: [{ id: "1" }],
+    };
+    mockFetchContent.mockResolvedValue(resp);
+
+    const id = 1;
+    const response = await client.fetchImage(id, queries);
+
+    expect(response).toEqual(resp);
+
+    // Ensure that fetchContent was called with the correct arguments
+    expect(fetchContent).toHaveBeenCalledWith(
+      baseURL,
+      apiPath,
+      `images/${id}`,
+      { ...queries },
+      { Authorization: "Bearer token" },
+      cache
+    );
+  });
+
+  it("should fetch a single document based on ID or slug", async () => {
+    // Mock the fetchContent function to return a successful response
+    const resp = {
+      meta: { total_count: 1 },
+      items: [{ id: "1" }],
+    };
+    mockFetchContent.mockResolvedValue(resp);
+
+    const id = 1;
+    const response = await client.fetchDocument(id, queries);
+
+    expect(response).toEqual(resp);
+
+    // Ensure that fetchContent was called with the correct arguments
+    expect(fetchContent).toHaveBeenCalledWith(
+      baseURL,
+      apiPath,
+      `documents/${id}`,
+      { ...queries },
+      { Authorization: "Bearer token" },
+      cache
+    );
+  });
+
+  it("should handle unsuccessful request for fetchPage", async () => {
+    // Mock the fetchRequest function to return an error response
+    mockFetchContent.mockRejectedValue(
+      new FetchError("Page not found", "REQUEST_FAILED")
+    );
+
+    const idOrSlug = "non-existent-page";
+
+    try {
+      await client.fetchPage(idOrSlug, queries);
+    } catch (error: any) {
+      expect(error).toBeInstanceOf(FetchError);
+      expect(error.code).toBe("REQUEST_FAILED");
+      expect(error.message).toBe("Page not found");
+    }
+
+    // Ensure that fetchContent was called with the correct arguments
+    expect(fetchContent).toHaveBeenCalledWith(
+      baseURL,
+      apiPath,
+      "pages",
+      { slug: idOrSlug, ...queries },
+      { Authorization: "Bearer token" },
+      cache
+    );
+  });
+
+  it("should handle unsuccessful request for fetchImage", async () => {
+    // Mock the fetchRequest function to return an error response
+    mockFetchContent.mockRejectedValue(
+      new FetchError("Image not found", "REQUEST_FAILED")
+    );
+
+    try {
+      await client.fetchImage(1, queries);
+    } catch (error: any) {
+      expect(error).toBeInstanceOf(FetchError);
+      expect(error.code).toBe("REQUEST_FAILED");
+      expect(error.message).toBe("Image not found");
+    }
+
+    // Ensure that fetchContent was called with the correct arguments
+    expect(fetchContent).toHaveBeenCalledWith(
+      baseURL,
+      apiPath,
+      `images/${1}`,
+      { ...queries },
+      { Authorization: "Bearer token" },
+      cache
+    );
+  });
+
+  it("should handle unsuccessful request for fetchDocument", async () => {
+    // Mock the fetchRequest function to return an error response
+    mockFetchContent.mockRejectedValue(
+      new FetchError("Document not found", "REQUEST_FAILED")
+    );
+
+    try {
+      await client.fetchDocument(1, queries);
+    } catch (error: any) {
+      expect(error).toBeInstanceOf(FetchError);
+      expect(error.code).toBe("REQUEST_FAILED");
+      expect(error.message).toBe("Document not found");
+    }
+
+    // Ensure that fetchContent was called with the correct arguments
+    expect(fetchContent).toHaveBeenCalledWith(
+      baseURL,
+      apiPath,
+      `documents/${1}`,
+      { ...queries },
+      { Authorization: "Bearer token" },
+      cache
+    );
+  });
+
+  it("should fetch all pages", async () => {
+    // Mock the fetchContent function to return a successful response
+    const resp = {
+      meta: { total_count: 1 },
+      items: [{ id: "1" }],
+    };
+    mockFetchContent.mockResolvedValue(resp);
+
+    const response = await client.fetchPages(queries);
+
+    expect(response).toEqual(resp);
+
+    // Ensure that fetchContent was called with the correct arguments
+    expect(fetchContent).toHaveBeenCalledWith(
+      baseURL,
+      apiPath,
+      "pages",
+      queries,
+      { Authorization: "Bearer token" },
+      cache
+    );
+  });
+
+  it("should fetch all images", async () => {
+    // Mock the fetchContent function to return a successful response
+    const resp = {
+      meta: { total_count: 1 },
+      items: [{ id: "1" }],
+    };
+    mockFetchContent.mockResolvedValue(resp);
+
+    const response = await client.fetchImages(queries);
+
+    expect(response).toEqual(resp);
+
+    // Ensure that fetchContent was called with the correct arguments
+    expect(fetchContent).toHaveBeenCalledWith(
+      baseURL,
+      apiPath,
+      "images",
+      queries,
+      { Authorization: "Bearer token" },
+      cache
+    );
+  });
+
+  it("should fetch all documents", async () => {
+    // Mock the fetchContent function to return a successful response
+    const resp = {
+      meta: { total_count: 1 },
+      items: [{ id: "1" }],
+    };
+    mockFetchContent.mockResolvedValue(resp);
+
+    const response = await client.fetchDocuments(queries);
+
+    expect(response).toEqual(resp);
+
+    // Ensure that fetchContent was called with the correct arguments
+    expect(fetchContent).toHaveBeenCalledWith(
+      baseURL,
+      apiPath,
+      "documents",
+      queries,
+      { Authorization: "Bearer token" },
+      cache
+    );
+  });
+
+  it("should construct the URL for a media item based on its type", () => {
+    const imageMedia: CMSMediaMeta = {
+      type: "wagtailimages.Image",
+      detail_url: "https://traleor.com/api/cms/v2/images/2/",
+      download_url: "/images/1/image.jpg",
+    };
+    const documentMedia: CMSMediaMeta = {
+      type: "wagtaildocs.Document",
+      detail_url: "https://example.com/docs/1/",
+    };
+
+    const imageSrc = client.getMediaSrc(imageMedia);
+    const documentSrc = client.getMediaSrc(documentMedia);
+    console.log("IMG", imageSrc);
+    console.log("DOC", documentSrc);
+
+    expect(imageSrc).toBe("https://api.example.com/images/1/image.jpg");
+    expect(documentSrc).toBe("https://api.example.com/docs/1/");
   });
 });
